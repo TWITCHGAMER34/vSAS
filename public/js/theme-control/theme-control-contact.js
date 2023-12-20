48771a2f0c87e1fe00c5feb51361a5127a48f014
Zoltan Nagy <nazoma05@gmail.com>
2023-12-20 11:35:14 +0100

    fixed so the textarea in contact-form is only resizeble vericaly and added word cap.

--- public/js/theme-control/theme-control-contact.js
+++ public/js/theme-control/theme-control-contact.js
@@ -6,8 +6,10 @@ const form = document.querySelector('form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 const label = document.querySelector('label');
+const p = document.querySelector('.dark');
+const count = document.querySelector('#word-count');
 
-const toChange = [document.body, container, form, input, textarea, label]
+const toChange = [document.body, container, form, input, textarea, label, p, count]
 
 function changeTheme(newTheme) {
     icon.style.rotate = newTheme === "dark" ? "180deg" : "0deg";
@@ -19,4 +21,22 @@ icon.addEventListener('click', function () {
     const newTheme = localStorage.getItem("theme") === "dark" ? "light" : "dark";
     localStorage.setItem("theme", newTheme)
     changeTheme(newTheme);
+});
+
+const message = document.getElementById('message');
+const wordCount = document.getElementById('word-count');
+
+message.addEventListener('input', function(event) {
+    const numChars = this.value.length;
+    const remaining = 500 - numChars;
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+
+    if (remaining < 0) {
+        event.preventDefault();
+        this.value = this.value.slice(0, 500);
+        wordCount.textContent = '0 characters remaining';
+    } else {
+        wordCount.textContent = `${remaining} characters remaining`;
+    }
 });
